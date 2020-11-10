43bf5af419da436f27ed5447bf590487152d7025
Amy Wooding <amy@wooding.org>
2020-11-10 10:22:37 -0700

    add test"

--- src/tests/no_ci/test_user_dataset_environment_integration.py
+++ src/tests/no_ci/test_user_dataset_environment_integration.py
@@ -10,5 +10,10 @@ class TestDatasets(unittest.TestCase):
     Basic smoke tests to ensure that all of the available datasets
     load and have some expected property.
     """
-    def basic_unit_test(self):
-        assert True
+    def test_wine_reviews_130k(self):
+        ds = Dataset.load('wine_reviews_130k')
+        assert ds.data.shape == (129971, 13)
+
+    def test_wine_reviews_150k(self):
+        ds = Dataset.load('wine_reviews_150k')
+        assert ds.data.shape == (150930, 10)
