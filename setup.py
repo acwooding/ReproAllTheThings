72deebb8385d470c18f0311600859dfe3cf7cc6c
Kjell Wooding <kjell@wooding.org>
2021-06-09 17:37:35 -0400

    sync with easydata2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='src',
     packages=find_packages(),
-    version='2.0.0a1',
+    version='2.0.0',
     description='''Reproducing EmbedAllTheThings by jc-healy and making it reproducible via easydata.''',
     author='John Healy, Amy Wooding',
     license='MIT',
